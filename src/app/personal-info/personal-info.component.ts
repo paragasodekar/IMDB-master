3152ed52f9f0cabcf61f6a34a5ecf1c5f31fff1a
paragasodekar <65756989+paragasodekar@users.noreply.github.com>
2020-09-04 13:59:18 +0530

    content grid

--- src/app/personal-info/personal-info.component.ts
+++ src/app/personal-info/personal-info.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-personal-info',
   templateUrl: './personal-info.component.html',
   styleUrls: ['./personal-info.component.css'],
+  providers: [NgbCarouselConfig],
 })
 export class PersonalInfoComponent implements OnInit {
-  list = [1, 2, 3, 4, 5, 5];
+  // list = [1];
+  images = ['assets/scroll.jpg', 'assets/scroll2.jpg', 'assets/scroll3.jpg', 'assets/scroll2.jpg'];
 
-  constructor() {}
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 10000;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+  }
 
   ngOnInit(): void {}
 }
